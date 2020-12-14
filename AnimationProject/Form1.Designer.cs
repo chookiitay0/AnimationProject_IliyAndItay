a6aea53218a85b2c80fc758f953c15cb919776ca
איתי שחר <itayshachar04@gmail.com>
2020-12-14 14:40:31 +0200

    Add project files.

---
+++ AnimationProject/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace AnimationProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FirstLetter = new System.Windows.Forms.Label();
+            this.SecondLetter = new System.Windows.Forms.Label();
+            this.ThirdLetter = new System.Windows.Forms.Label();
+            this.FourthLetter = new System.Windows.Forms.Label();
+            this.FifthLetter = new System.Windows.Forms.Label();
+            this.SixthLetter = new System.Windows.Forms.Label();
+            this.SeventhLetter = new System.Windows.Forms.Label();
+            this.EighthLetter = new System.Windows.Forms.Label();
+            this.NinthLetter = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.roni = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.timer10 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.roni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FirstLetter
+            // 
+            this.FirstLetter.BackColor = System.Drawing.Color.White;
+            this.FirstLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FirstLetter.ForeColor = System.Drawing.Color.Black;
+            this.FirstLetter.Location = new System.Drawing.Point(630, 200);
+            this.FirstLetter.Name = "FirstLetter";
+            this.FirstLetter.Size = new System.Drawing.Size(73, 60);
+            this.FirstLetter.TabIndex = 1;
+            this.FirstLetter.Text = "ח";
+            this.FirstLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.FirstLetter.Click += new System.EventHandler(this.FirstLetter_Click);
+            // 
+            // SecondLetter
+            // 
+            this.SecondLetter.BackColor = System.Drawing.Color.White;
+            this.SecondLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.SecondLetter.ForeColor = System.Drawing.Color.Black;
+            this.SecondLetter.Location = new System.Drawing.Point(551, 200);
+            this.SecondLetter.Name = "SecondLetter";
+            this.SecondLetter.Size = new System.Drawing.Size(73, 60);
+            this.SecondLetter.TabIndex = 2;
+            this.SecondLetter.Text = "נ";
+            this.SecondLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ThirdLetter
+            // 
+            this.ThirdLetter.BackColor = System.Drawing.Color.White;
+            this.ThirdLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ThirdLetter.ForeColor = System.Drawing.Color.Black;
+            this.ThirdLetter.Location = new System.Drawing.Point(472, 200);
+            this.ThirdLetter.Name = "ThirdLetter";
+            this.ThirdLetter.Size = new System.Drawing.Size(73, 60);
+            this.ThirdLetter.TabIndex = 3;
+            this.ThirdLetter.Text = "ו";
+            this.ThirdLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FourthLetter
+            // 
+            this.FourthLetter.BackColor = System.Drawing.Color.White;
+            this.FourthLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FourthLetter.ForeColor = System.Drawing.Color.Black;
+            this.FourthLetter.Location = new System.Drawing.Point(393, 200);
+            this.FourthLetter.Name = "FourthLetter";
+            this.FourthLetter.Size = new System.Drawing.Size(73, 60);
+            this.FourthLetter.TabIndex = 4;
+            this.FourthLetter.Text = "כ";
+            this.FourthLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FifthLetter
+            // 
+            this.FifthLetter.BackColor = System.Drawing.Color.White;
+            this.FifthLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FifthLetter.ForeColor = System.Drawing.Color.Black;
+            this.FifthLetter.Location = new System.Drawing.Point(314, 54);
+            this.FifthLetter.Name = "FifthLetter";
+            this.FifthLetter.Size = new System.Drawing.Size(73, 60);
+            this.FifthLetter.TabIndex = 5;
+            this.FifthLetter.Text = "ה";
+            this.FifthLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SixthLetter
+            // 
+            this.SixthLetter.BackColor = System.Drawing.Color.White;
+            this.SixthLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.SixthLetter.ForeColor = System.Drawing.Color.Black;
+            this.SixthLetter.Location = new System.Drawing.Point(630, 50);
+            this.SixthLetter.Name = "SixthLetter";
+            this.SixthLetter.Size = new System.Drawing.Size(73, 60);
+            this.SixthLetter.TabIndex = 6;
+            this.SixthLetter.Text = "ש";
+            this.SixthLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SixthLetter.Click += new System.EventHandler(this.FirstLetter_Click);
+            // 
+            // SeventhLetter
+            // 
+            this.SeventhLetter.BackColor = System.Drawing.Color.White;
+            this.SeventhLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.SeventhLetter.ForeColor = System.Drawing.Color.Black;
+            this.SeventhLetter.Location = new System.Drawing.Point(551, 50);
+            this.SeventhLetter.Name = "SeventhLetter";
+            this.SeventhLetter.Size = new System.Drawing.Size(73, 60);
+            this.SeventhLetter.TabIndex = 7;
+            this.SeventhLetter.Text = "מ";
+            this.SeventhLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SeventhLetter.Click += new System.EventHandler(this.SeventhLetter_Click);
+            // 
+            // EighthLetter
+            // 
+            this.EighthLetter.BackColor = System.Drawing.Color.White;
+            this.EighthLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.EighthLetter.ForeColor = System.Drawing.Color.Black;
+            this.EighthLetter.Location = new System.Drawing.Point(472, 50);
+            this.EighthLetter.Name = "EighthLetter";
+            this.EighthLetter.Size = new System.Drawing.Size(73, 60);
+            this.EighthLetter.TabIndex = 8;
+            this.EighthLetter.Text = "ח";
+            this.EighthLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NinthLetter
+            // 
+            this.NinthLetter.BackColor = System.Drawing.Color.White;
+            this.NinthLetter.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.NinthLetter.ForeColor = System.Drawing.Color.Black;
+            this.NinthLetter.Location = new System.Drawing.Point(393, 50);
+            this.NinthLetter.Name = "NinthLetter";
+            this.NinthLetter.Size = new System.Drawing.Size(73, 60);
+            this.NinthLetter.TabIndex = 9;
+            this.NinthLetter.Text = "!";
+            this.NinthLetter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 170;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 200;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 180;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Enabled = true;
+            this.timer5.Interval = 200;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Enabled = true;
+            this.timer6.Interval = 150;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Enabled = true;
+            this.timer7.Interval = 175;
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // timer8
+            // 
+            this.timer8.Enabled = true;
+            this.timer8.Interval = 150;
+            this.timer8.Tick += new System.EventHandler(this.timer8_Tick);
+            // 
+            // timer9
+            // 
+            this.timer9.Enabled = true;
+            this.timer9.Tick += new System.EventHandler(this.timer9_Tick);
+            // 
+            // roni
+            // 
+            this.roni.Image = global::AnimationProject.Properties.Resources.RoniDesert;
+            this.roni.Location = new System.Drawing.Point(152, 402);
+            this.roni.Name = "roni";
+            this.roni.Size = new System.Drawing.Size(152, 129);
+            this.roni.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.roni.TabIndex = 10;
+            this.roni.TabStop = false;
+            // 
+            // background
+            // 
+            this.background.Image = global::AnimationProject.Properties.Resources.background3;
+            this.background.Location = new System.Drawing.Point(-1, -1);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(1213, 575);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            this.background.Click += new System.EventHandler(this.background_Click);
+            // 
+            // timer10
+            // 
+            this.timer10.Enabled = true;
+            this.timer10.Tick += new System.EventHandler(this.timer10_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 568);
+            this.Controls.Add(this.roni);
+            this.Controls.Add(this.NinthLetter);
+            this.Controls.Add(this.EighthLetter);
+            this.Controls.Add(this.SeventhLetter);
+            this.Controls.Add(this.SixthLetter);
+            this.Controls.Add(this.FifthLetter);
+            this.Controls.Add(this.FourthLetter);
+            this.Controls.Add(this.ThirdLetter);
+            this.Controls.Add(this.SecondLetter);
+            this.Controls.Add(this.FirstLetter);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.roni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label FirstLetter;
+        private System.Windows.Forms.Label SecondLetter;
+        private System.Windows.Forms.Label ThirdLetter;
+        private System.Windows.Forms.Label FourthLetter;
+        private System.Windows.Forms.Label FifthLetter;
+        private System.Windows.Forms.Label SixthLetter;
+        private System.Windows.Forms.Label SeventhLetter;
+        private System.Windows.Forms.Label EighthLetter;
+        private System.Windows.Forms.Label NinthLetter;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox roni;
+        private System.Windows.Forms.Timer timer10;
+    }
+}
+
---
+++ AnimationProject/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimationProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FirstLetter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (SixthLetter.Top <= 125)
+            {
+                SixthLetter.Top += 5;
+
+                if (SixthLetter.Top == 125)
+                {
+                    timer1.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    SixthLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    SixthLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    SixthLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    SixthLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SeventhLetter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (SeventhLetter.Top <= 125)
+            {
+                SeventhLetter.Top += 5;
+
+                if (SeventhLetter.Top == 125)
+                {
+                    timer2.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    SeventhLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    SeventhLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    SeventhLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    SeventhLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (EighthLetter.Top <= 125)
+            {
+                EighthLetter.Top += 5;
+
+                if (EighthLetter.Top == 125)
+                {
+                    timer3.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    EighthLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    EighthLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    EighthLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    EighthLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (NinthLetter.Top <= 125)
+            {
+                NinthLetter.Top += 5;
+
+                if (NinthLetter.Top == 125)
+                {
+                    timer4.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    NinthLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    NinthLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    NinthLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    NinthLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+
+            if (FirstLetter.Top >= 35)
+            {
+                FirstLetter.Top -= 5;
+
+                if (FirstLetter.Top == 35)
+                {
+                    timer5.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    FirstLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    FirstLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    FirstLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    FirstLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            if (SecondLetter.Top >= 35)
+            {
+                SecondLetter.Top -= 5;
+
+                if (SecondLetter.Top == 35)
+                {
+                    timer6.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    SecondLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    SecondLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    SecondLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    SecondLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            if (ThirdLetter.Top >= 35)
+            {
+                ThirdLetter.Top -= 5;
+
+                if (ThirdLetter.Top == 35)
+                {
+                    timer7.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    ThirdLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    ThirdLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    ThirdLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    ThirdLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+            if (FourthLetter.Top >= 35)
+            {
+                FourthLetter.Top -= 5;
+
+                if (FourthLetter.Top == 35)
+                {
+                    timer8.Stop();
+                }
+            }
+
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    FourthLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    FourthLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    FourthLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    FourthLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer9_Tick(object sender, EventArgs e)
+        {
+            if (roni.Height <= 150)
+            {
+                roni.Height += 10;
+                roni.Top -= 10;
+                roni.Width += 10;
+                roni.Left -= 10;
+            }
+            else
+            {
+                roni.Height = 50;
+                roni.Top = 300;
+                roni.Width = 50;
+                roni.Left = 150;
+            }
+        }
+
+        private void timer10_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    FifthLetter.ForeColor = Color.Red;
+                    break;
+                case 2:
+
+                    FifthLetter.ForeColor = Color.Green;
+                    break;
+                case 3:
+
+                    FifthLetter.ForeColor = Color.Blue;
+                    break;
+                case 4:
+
+                    FifthLetter.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
